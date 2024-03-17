757b80a06c6ea5cf1380154c09943864e366a0d3
janf <ioan.ferencik@solargis.com>
2024-03-17 19:35:49 +0100

    clean up

--- t.py
+++
@@ -1,9 +0,0 @@
-import os
-from nighttimelights_pipeline.utils import fetch_resource_size
-local = '/tmp/SVDNB_npp_d20240125.rade9d.tif.local'
-#local = '/work/tmp/ntl/SVDNB_npp_d20240125.rade9d.tif'
-#latest = '/work/tmp/ntl/SVDNB_npp_d20240125.rade9d_latest.tif'
-remote = 'https://eogdata.mines.edu/nighttime_light/nightly/rade9d/SVDNB_npp_d20240125.rade9d.tif'
-
-
-print (os.path.getsize(local),  fetch_resource_size(remote))
