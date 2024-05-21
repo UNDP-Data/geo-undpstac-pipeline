7e22876d550c47a97a7e0fcb07867550a216e6b9
janf <ioan.ferencik@solargis.com>
2024-05-21 17:16:45 +0200

    save items individually using save_bject

--- undpstac_pipeline/fix.py
+++ undpstac_pipeline/fix.py
@@ -9,8 +9,7 @@ from undpstac_pipeline.stac import AzureStacIO, CustomLayoutStrategy, item_f, ca
 
 logger = logging.getLogger(__name__)
 
-
-def update_stac_items():
+def update_stac_items(dry_run=False):
     az_stacio = AzureStacIO()
 
     root_catalog_blob_path = const.STAC_CATALOG_NAME
@@ -22,28 +21,45 @@ def update_stac_items():
     nighttime_collection = root_catalog.get_child(const.AZURE_DNB_COLLECTION_FOLDER)
     for item in nighttime_collection.get_all_items():
         item_date = item.datetime.date()
-        # if item_date.strftime('%Y-%m-%d') != '2024-01-01':
-        #     logger.info(f'skipped {str(item_date)}')
-        #     continue
         logger.info(f'checking {str(item_date)}')
-        for asset_name in ('DNB', 'DNB_SUNFILTERED'):
+        for asset_name in ('DNB_SUNFILTERED', 'DNB'):
             if asset_name not in item.assets:
                 continue
-            try:
-                remote_dnb_url = compute_dnb_filename(date=item_date, file_type=asset_name)
-                original_size_bytes = fetch_resource_size(url=remote_dnb_url)
-                asset = item.assets[asset_name]
-                asset.extra_fields['original_file_size'] = original_size_bytes
-                logger.debug(f'Updated original_file_size of {asset_name} to {original_size_bytes} in {str(item_date)}')
-            except KeyError:
-                pass
+            remote_dnb_url = compute_dnb_filename(date=item_date, file_type=asset_name)
+            remote_size_bytes = fetch_resource_size(url=remote_dnb_url)
+            asset = item.assets[asset_name]
 
-    logger.info('Saving STAC structure to Azure')
-    root_catalog.normalize_hrefs(root_href=az_stacio.container_client.url,
-                                 strategy=CustomLayoutStrategy(catalog_func=catalog_f, item_func=item_f))
+            if not 'original_size_bytes' in asset.extra_fields:
+                if not dry_run:
+                    asset.extra_fields['original_size_bytes'] = remote_size_bytes
+                    logger.info(
+                        f'Created original_size_bytes={remote_size_bytes} property for {asset_name} - {item_date}')
+                else:
+                    logger.info(f'Ought to create original_size_bytes={remote_size_bytes} property for {asset_name} - {item_date}')
+            else:
+                original_size_bytes = asset.extra_fields['original_size_bytes']
+                if original_size_bytes != remote_size_bytes:
+                    if dry_run:
+                        logger.info(f'Ought to change original_size_bytes from {original_size_bytes} to {remote_size_bytes} for {item_date}-{asset_name}')
+                    else:
+                        logger.info(
+                            f'Replaced original_size_bytes from {original_size_bytes} with {remote_size_bytes} for {item_date}-{asset_name}')
+                        asset.extra_fields['original_size_bytes'] = remote_size_bytes
+                else:
+                    logger.info(f'original_size_bytes for {item_date}-{asset_name} asset needs no change')
+            if not dry_run:
+                logger.info(f'Saving item {item_date}')
+                item.make_asset_hrefs_relative()
+                item.save_object(include_self_link=False, stac_io=az_stacio)
+            # logger.debug(f'Updated original_file_size of {asset_name} to {original_size_bytes} in {str(item_date)}')
 
-    root_catalog.save(stac_io=az_stacio)
+
+
+
 
 
 if __name__ == '__main__':
-    update_stac_items()
+    logging.basicConfig()
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
+    update_stac_items(dry_run=False)
